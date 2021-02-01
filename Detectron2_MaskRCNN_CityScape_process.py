11d1544cc7e553285be5841ec9c4227881ad4eab
Ludo <ludovic.barusseau@gmail.com>
2021-02-01 15:53:03 +0100

    Initial commit

---
+++ Detectron2_MaskRCNN_CityScape_process.py
@@ -0,0 +1,229 @@
+import update_path
+from ikomia import core, dataprocess
+import copy
+# Your imports below
+import torch
+from detectron2 import model_zoo
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+from detectron2.data import MetadataCatalog
+import random
+
+
+# --------------------
+# - Class to handle the process parameters
+# - Inherits core.CProtocolTaskParam from Ikomia API
+# --------------------
+class Detectron2_MaskRCNN_CityScapeParam(core.CProtocolTaskParam):
+
+    def __init__(self):
+        core.CProtocolTaskParam.__init__(self)
+        self.cuda = True
+        self.proba = 0.8
+
+    def setParamMap(self, paramMap):
+        self.cuda = int(paramMap["cuda"])
+        self.proba = int(paramMap["proba"])
+
+    def getParamMap(self):
+        paramMap = core.ParamMap()
+        paramMap["cuda"] = str(self.cuda)
+        paramMap["proba"] = str(self.proba)
+        return paramMap
+
+# --------------------
+# - Class which implements the process
+# - Inherits core.CProtocolTask or derived from Ikomia API
+# --------------------
+class Detectron2_MaskRCNN_CityScapeProcess(dataprocess.CImageProcess2d):
+
+    def __init__(self, name, param):
+        dataprocess.CImageProcess2d.__init__(self, name)
+        if param is None:
+            self.setParam(Detectron2_MaskRCNN_CityScapeParam())
+        else:
+            self.setParam(copy.deepcopy(param))
+
+        # get and set config model
+        self.LINK_MODEL = "Cityscapes/mask_rcnn_R_50_FPN.yaml"
+        self.threshold = 0.5
+        self.cfg = get_cfg()
+        self.cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = self.threshold
+        self.cfg.merge_from_file(model_zoo.get_config_file(self.LINK_MODEL)) # load config from file(.yaml)
+        self.cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(self.LINK_MODEL) # download the model (.pkl)
+        self.loaded = False
+        self.deviceFrom = ""
+
+        # add Outputs :
+        self.setOutputDataType(core.IODataType.IMAGE_LABEL, 0)
+        self.addOutput(dataprocess.CImageProcessIO(core.IODataType.IMAGE))
+        self.addOutput(dataprocess.CGraphicsOutput())
+
+    def getProgressSteps(self, eltCount=1):
+        # Function returning the number of progress steps for this process
+        # This is handled by the main progress bar of Ikomia application
+        return 3
+
+    def run(self):
+        self.beginTaskRun()
+
+        # we use seed to keep the same color for our masks + boxes + labels (same random each time)
+        random.seed(30)
+
+        # Get input :
+        input = self.getInput(0)
+        srcImage = input.getImage()
+
+        # Get output :
+        mask_output = self.getOutput(0)
+        output_graph = self.getOutput(2)
+        output_graph.setImageIndex(1)
+        output_graph.setNewLayer("CityScapeMaskRCNN")
+
+        # Get parameters :
+        param = self.getParam()
+
+        # predictor
+        if not self.loaded:
+            print("Chargement du modèle")
+            if param.cuda == False:
+                self.cfg.MODEL.DEVICE = "cpu"
+                self.deviceFrom = "cpu"
+            else:
+                self.deviceFrom = "gpu"
+            self.predictor = DefaultPredictor(self.cfg)
+            self.loaded = True
+        # reload model if CUDA check and load without CUDA 
+        elif self.deviceFrom == "cpu" and param.cuda == True:
+            print("Chargement du modèle")
+            self.cfg = get_cfg()
+            self.cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = self.threshold
+            self.cfg.merge_from_file(model_zoo.get_config_file(self.LINK_MODEL)) # load config from file(.yaml)
+            self.cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(self.LINK_MODEL) # download the model (.pkl)
+            self.predictor = DefaultPredictor(self.cfg)
+            self.deviceFrom = "gpu"
+        # reload model if CUDA not check and load with CUDA
+        elif self.deviceFrom == "gpu" and param.cuda == False:
+            print("Chargement du modèle")
+            self.cfg = get_cfg()
+            self.cfg.MODEL.DEVICE = "cpu"
+            self.cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = self.threshold
+            self.cfg.merge_from_file(model_zoo.get_config_file(self.LINK_MODEL)) # load config from file(.yaml)
+            self.cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(self.LINK_MODEL) # download the model (.pkl)
+            self.predictor = DefaultPredictor(self.cfg)
+            self.deviceFrom = "cpu"
+
+        outputs = self.predictor(srcImage)
+        
+        # get outputs instances
+        boxes = outputs["instances"].pred_boxes
+        scores = outputs["instances"].scores
+        classes = outputs["instances"].pred_classes
+        masks = outputs["instances"].pred_masks
+
+        # to numpy
+        if param.cuda :
+            boxes_np = boxes.tensor.cpu().numpy()
+            scores_np = scores.cpu().numpy()
+            classes_np = classes.cpu().numpy()
+        else :
+            boxes_np = boxes.tensor.numpy()
+            scores_np = scores.numpy()
+            classes_np = classes.numpy()
+
+        self.emitStepProgress()
+
+        # keep only the results with proba > threshold
+        scores_np_tresh = list()
+        for s in scores_np:
+            if float(s) > param.proba:
+                scores_np_tresh.append(s)
+
+        if len(scores_np_tresh) > 0 :
+            # create random color for masks + boxes + labels
+            colors = [[0,0,0]]
+            for i in range(len(scores_np_tresh)):
+                colors.append([random.randint(0,255), random.randint(0,255), random.randint(0,255), 255])
+
+            # text labels with scores
+            labels = None
+            class_names = MetadataCatalog.get(self.cfg.DATASETS.TRAIN[0]).get("thing_classes")
+            if classes is not None and class_names is not None and len(class_names) > 1:
+                labels = [class_names[i] for i in classes]
+            if scores_np_tresh is not None:
+                if labels is None:
+                    labels = ["{:.0f}%".format(s * 100) for s in scores_np_tresh]
+                else:
+                    labels = ["{} {:.0f}%".format(l, s * 100) for l, s in zip(labels, scores_np_tresh)]
+
+            # Show boxes + labels
+            for i in range(len(scores_np_tresh)):
+                properties_text = core.GraphicsTextProperty() 
+                properties_text.color = colors[i+1] # start with i+1 we don't use the first color dedicated for the label mask
+                properties_text.font_size = 7
+                properties_rect = core.GraphicsRectProperty()
+                properties_rect.pen_color = colors[i+1]
+                output_graph.addRectangle(float(boxes_np[i][0]), float(boxes_np[i][1]), float(boxes_np[i][2] - boxes_np[i][0]), float(boxes_np[i][3] - boxes_np[i][1]), properties_rect)
+                output_graph.addText(labels[i],float(boxes_np[i][0]), float(boxes_np[i][1]),properties_text)
+
+            self.emitStepProgress()
+
+            # label mask
+            nb_objects = len(masks[:len(scores_np_tresh)]) 
+            if nb_objects > 0:
+                masks = masks[:nb_objects, :, :, None]
+                mask_or = masks[0]*nb_objects
+                for j in range(1, nb_objects):
+                    mask_or = torch.max(mask_or, masks[j] * (nb_objects-j))
+                mask_numpy = mask_or.byte().cpu().numpy()
+                mask_output.setImage(mask_numpy)
+
+                # output mask apply to our original image 
+                # inverse colors to match boxes colors
+                c = colors[1:]
+                c = c[::-1]
+                colors = [[0,0,0]]
+                for col in c:
+                    colors.append(col)
+                self.setOutputColorMap(1, 0, colors)
+        else:
+            self.emitStepProgress()
+
+        self.forwardInputImage(0, 1)
+
+        # Step progress bar:
+        self.emitStepProgress()
+
+        # Call endTaskRun to finalize process
+        self.endTaskRun()
+
+
+# --------------------
+# - Factory class to build process object
+# - Inherits dataprocess.CProcessFactory from Ikomia API
+# --------------------
+class Detectron2_MaskRCNN_CityScapeProcessFactory(dataprocess.CProcessFactory):
+
+    def __init__(self):
+        dataprocess.CProcessFactory.__init__(self)
+        self.info.name = "Detectron2_MaskRCNN_CityScape"
+        self.info.shortDescription = "Mask R-CNN inference model of Detectron2 for object detection and segmentation."
+        self.info.description = "Mask R-CNN inference model for object detection and segmentation. " \
+                                "Implementation from Detectron2 (Facebook Research). " \
+                                "This Ikomia plugin can make inference of pre-trained model from " \
+                                "COCO and CityScapes datasets."
+        self.info.authors = "Kaiming He, Georgia Gkioxari, Piotr Dollar, Ross Girshick"
+        self.info.article = "Mask R-CNN"
+        self.info.journal = "Proceedings of the IEEE International Conference on Computer Vision (ICCV)"
+        self.info.year = 2017
+        self.info.license = "Apache-2.0 License"
+        self.info.documentationLink = "https://detectron2.readthedocs.io/index.html"
+        self.info.repo = "https://github.com/facebookresearch/detectron2"
+        self.info.path = "Plugins/Python/Detectron2"
+        self.info.iconPath = "icons/detectron2.png"
+        self.info.version = "1.0.0"
+        self.info.keywords = "mask,rcnn,maskRCNN,facebook,detectron2,detection,segmentation"
+
+    def create(self, param=None):
+        # Create process object
+        return Detectron2_MaskRCNN_CityScapeProcess(self.info.name, param)
